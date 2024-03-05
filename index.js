c29aab41815cf3c4744dc39da08392d960b0cb1b
igordjasic <119559990+igordjasic@users.noreply.github.com>
2024-03-04 16:00:48 -0800

    final commit

--- index.js
+++ index.js
@@ -56,3 +56,25 @@ const shuffle = (list) => {
     - Append an exclamation point (!) to the .textContent property of the h1 on the page
 
 */
+const urls = [
+    'https://i.ibb.co/sjvVrG3/fruit-plyh.jpg',
+    'https://i.ibb.co/g3tqj4w/fruit-zxcv.jpg',
+    'https://i.ibb.co/88Hy4rD/fruit-wert.jpg',
+    'https://i.ibb.co/NmNMLKF/fruit-astt.jpg',
+    'https://i.ibb.co/qp8Q1G6/fruit-dfgh.jpg',
+    'https://i.ibb.co/9thfVhB/fruit-hjkl.jpg',
+    'https://i.ibb.co/YtTfqvD/fruit-dcnj.jpg',
+    'https://i.ibb.co/CVwKM8Z/fruit-ecuh.jpg',
+    'https://i.ibb.co/ccNyKX0/fruit-uzxc.jpg'
+];
+
+const randomUrls = shuffle(urls);
+
+const images = document.querySelectorAll('.card img');
+
+images.forEach((image, index) => {
+    image.src = randomUrls[index];
+});
+
+const h1 = document.querySelector('h1');
+h1.textContent += '!';
